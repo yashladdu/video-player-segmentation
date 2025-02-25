737c557fa54a263321335e48d8823f9aa80cd47d
Yash Laddu <144030942+yashladdu@users.noreply.github.com>
2025-02-25 13:59:46 +0400

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -28,7 +28,7 @@ const Cards = ({video}) => {
 
   useEffect(() => {
       const fetchChannel = async () => {
-        const res = await axios.get(`/users/find/${video.userId}`);
+        const res = await axios.get(`https://video-player-backend-g2ot.onrender.com/api/users/find/${video.userId}`);
         setChannel(res.data);
       }
       fetchChannel()
